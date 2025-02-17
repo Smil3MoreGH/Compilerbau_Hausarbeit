f436bf33153300d723976e738c4df2265f725bac
Smil3MoreGH <paul.hart98@googlemail.com>
2025-02-17 18:52:29 +0100

    TokenType klasse gebaut, mit modernerer enum variante!

--- src/main/java/de/paul/compilerbau/scanner/TokenType.java
+++ src/main/java/de/paul/compilerbau/scanner/TokenType.java
@@ -1,4 +1,21 @@
 package de.paul.compilerbau.scanner;
 
-public class TokenType {
+public enum TokenType {
+    // Schlüsselwörter
+    VAR, FUN, RETURN, IF, ELSE, WHILE,
+
+    // Operatoren
+    PLUS, MINUS, MULT, DIV, ASSIGN,
+
+    // Vergleichsoperatoren
+    EQ, NEQ, LT, GT, LTE, GTE,
+
+    // Struktur
+    LPAREN, RPAREN, LBRACE, RBRACE, SEMI, COMMA,
+
+    // Werte
+    NUMBER, IDENTIFIER,
+
+    // Sonstiges
+    EOF  // Ende der Datei
 }
